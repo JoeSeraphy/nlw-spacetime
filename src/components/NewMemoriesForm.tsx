15f63489a0bc32e317f321a667b7a08e817ef769
joelson Silva <joelson.s@outlook.com>
2023-10-17 18:48:44 -0300

    add button

--- src/components/NewMemoriesForm.tsx
+++ src/components/NewMemoriesForm.tsx
@@ -79,6 +79,12 @@ export default function NewMemoriesForm() {
           placeholder="Fique livre para adicionar fotos, vídeos e ralatos sobre essa experiência que quer lembrar para sempre"
           className=" w-full flex-1 resize-none rounded border-0 bg-transparent p-0 text-lg leading-relaxed text-gray-100 placeholder:text-gray-400 focus:ring-0"
         />
+        <button
+          className="inline-block self-end rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black transition-colors hover:bg-green-600"
+          type="submit"
+        >
+          Salvar
+        </button>
       </form>
     </>
   );
